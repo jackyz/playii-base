ef77a065037e99d7dca17c931510d0239a659094
jackyz <jackyz.zhao@gmail.com>
2009-05-13 22:39:24 +0800

    miner fix

--- logic/ran/main.js
+++ logic/ran/main.js
@@ -1,71 +1,70 @@
 (function(){
 
    function idle(){
-     debug("ruan main idle");
    }
 
    function enter(who, param, data){
      this.ul[who] = param;
      this.uc ++;
+     debug("enter:: "+who+" , "+param+" #"+this.uc);
      pick.call(this, who);
    }
 
    function pick(who){
      var one = this.ll[who];
-     if (one != undefined){
-       error("pick:"+who+" double click?");
-       return;
-     }
-     one = this.pl.shift();
      if (one == undefined){
-       this.pl.push(who);
-       debug("pick:"+who+" wait");
-     } else {
-       this.ll[who] = one;
-       this.ll[one] = who;
-       this.lc ++;
-       cast(who)("bepick")(this.uc, this.lc, this.ul[one]);
-       cast(one)("bepick")(this.uc, this.lc, this.ul[who]);
-       debug("pick:"+who+" mate "+one);
+       one = this.wu;
+       if (one == undefined){
+	 this.wu = who;
+         debug("pick:: "+who+" wait");
+       } else if(one != who){
+	 this.wu = undefined;
+         this.ll[who] = one;
+         this.ll[one] = who;
+         this.lc ++;
+         cast(who)("bepick")(this.uc, this.lc, this.ul[one]);
+         cast(one)("bepick")(this.uc, this.lc, this.ul[who]);
+         debug("pick:: "+who+" - "+one+" #"+this.lc);
+       }
      }
    }
 
    function talk(who, what){
      var one = this.ll[who];
-     if (one == undefined){
-       error("talk:"+who+" double click?");
-     } else {
+     if (one != undefined){
        cast(one)("betold")(what);
-       debug("talk:"+who+" "+one+" "+what);
+       debug("talk:: "+who+" > "+one+" : "+what);
+     } else {
+       cast(one)("bebore")(this.uc, this.lc);
+       debug("talk:: "+who+" bebore");
      }
    }
 
    function bore(who){
      var one = this.ll[who];
-     if (one == undefined){
-       error("bore:"+who+" double click?");
-     } else {
-       delete this.ll[who];
-       delete this.ll[one];
+     if(one != undefined){
        this.lc --;
+       delete this.ll[one];
        cast(one)("bebore")(this.uc, this.lc);
-       debug("bore:"+who+" "+one);
      }
+     delete this.ll[who];
+     debug("bore:: "+who+" x "+one+" #"+this.lc);
    }
 
    function leave(who){
-     if(this.ll[who]) bore.call(this, who);
-     this.uc --;
+     bore.call(this, who);
+     if(this.wu == who) this.wu = undefined;
      delete this.ul[who];
+     this.uc --;
+     debug("leave:: "+who+" #"+this.uc);
    }
 
    return mixin(
-     // constructor
      function(parent, self){
-       this.parent = parent;
-       this.self = self;
+       this.parent = parent; // name of parent
+       this.self = self; // name of self
        this.ul = {}; // user list
-       this.pl = []; // pick list
+       this.wu = undefined; // wait user
        this.ll = {}; // link list
        this.uc = 0;  // user count
        this.lc = 0;  // link count
